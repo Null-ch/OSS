00dd96bdaff3d7196b67c87eb12226029f3a3014
Aleksandr Gilev <65172872+Null-ch@users.noreply.github.com>
2024-01-29 17:00:06 +0300

    added accoun repository

---
+++ resources/views/account/index.blade.php
@@ -0,0 +1,31 @@
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="/" class="nav-link">Главная</a>
+        </li>
+    </ul>
+
+    <ul class="navbar-nav ml-auto">
+        @if (auth()->check())
+            <li class="nav-item dropdown ml-auto">
+                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
+                    <span>{{ Auth::user()->name }}</span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right" style="left: inherit; right: 0px;">
+                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                           document.getElementById('logout-form').submit();">
+                        {{ __('Выйти') }}
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+            </li>
+        @endif
+    </ul>
+</nav>
+Здесь будет пользовательские страницы
