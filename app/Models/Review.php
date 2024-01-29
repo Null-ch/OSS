6e59a1ca9f35e9088aac448e31c1bb87e88ca603
Aleksandr Gilev <65172872+Null-ch@users.noreply.github.com>
2024-01-29 11:33:39 +0300

    init commit

---
+++ app/Models/Review.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Review extends Model
+{
+    use HasFactory, SoftDeletes;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'content',
+    ];
+
+    protected $table = 'reviews';
+    protected $guarded = false;
+
+}
