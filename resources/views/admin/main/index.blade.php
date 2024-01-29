96752cf2cb81e0a5d0fa66a7010411a22ffdcf12
Aleksandr Gilev <65172872+Null-ch@users.noreply.github.com>
2024-01-29 16:53:33 +0300

    added admin layouts

--- resources/views/admin/main/index.blade.php
+++ resources/views/admin/main/index.blade.php
@@ -3,58 +3,92 @@
     <div class="content-wrapper">
         <div class="content-header">
             <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                <h1 class="m-0">Панель администратора</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Главная</a></li>
-                    <li class="breadcrumb-item active">Панель администратора</li>
-                </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Панель администратора</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Главная</a></li>
+                            <li class="breadcrumb-item active">Панель администратора</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
             </div><!-- /.container-fluid -->
         </div>
         <section class="content">
             <div class="container-fluid">
-                <div class="row">
-                    <!-- Карточка Пользователей -->
-                    <div class="col-lg-3 col-6">
-                        <div class="small-box bg-info">
-                            <div class="inner" style="text-align: center">
-                                <p>Пользователи</p>
-                                {{-- <h3>{{ $users->count() }}</h3> --}}
+                <div>
+                    <h3>Магазин</h3>
+                    <div class="row">
+                        <!-- Карточка Пользователей -->
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-info">
+                                <div class="inner" style="text-align: center">
+                                    <p>Корзины</p>
+                                    {{-- <h3>{{ $users->count() }}</h3> --}}
+                                </div>
+                                {{-- <a href="{{ route('admin.users.index') }}" class="small-box-footer">Перейти к пользователям<i
+                                        class="fas fa-arrow-circle-right" style="margin-left: 5px"></i></a> --}}
                             </div>
-                            {{-- <a href="{{ route('admin.users.index') }}" class="small-box-footer">Перейти к пользователям<i
-                                    class="fas fa-arrow-circle-right" style="margin-left: 5px"></i></a> --}}
+                        </div>
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-info">
+                                <div class="inner" style="text-align: center">
+                                    <p>Категории</p>
+                                    {{-- <h3>{{ $users->count() }}</h3> --}}
+                                </div>
+                                {{-- <a href="{{ route('admin.users.index') }}" class="small-box-footer">Перейти к пользователям<i
+                                        class="fas fa-arrow-circle-right" style="margin-left: 5px"></i></a> --}}
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-info">
+                                <div class="inner" style="text-align: center">
+                                    <p>Заказы</p>
+                                    {{-- <h3>{{ $users->count() }}</h3> --}}
+                                </div>
+                                {{-- <a href="{{ route('admin.users.index') }}" class="small-box-footer">Перейти к пользователям<i
+                                        class="fas fa-arrow-circle-right" style="margin-left: 5px"></i></a> --}}
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-info">
+                                <div class="inner" style="text-align: center">
+                                    <p>Товары</p>
+                                    {{-- <h3>{{ $users->count() }}</h3> --}}
+                                </div>
+                                {{-- <a href="{{ route('admin.users.index') }}" class="small-box-footer">Перейти к пользователям<i
+                                        class="fas fa-arrow-circle-right" style="margin-left: 5px"></i></a> --}}
+                            </div>
                         </div>
                     </div>
-                    <!-- Карточка Постов -->
-                    {{-- <div class="col-lg-3 col-6">
-                        <div class="small-box bg-info">
-                            <div class="inner" style="text-align: center">
-                                <p>Статьи</p>
-                                <h3>{{ $posts->count() }}</h3>
+                </div>
+                <div>
+                    <h3>Сообщество</h3>
+                    <div class="row">
+                        <!-- Карточка Пользователей -->
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-info">
+                                <div class="inner" style="text-align: center">
+                                    <p>Отзывы</p>
+                                    {{-- <h3>{{ $users->count() }}</h3> --}}
+                                </div>
+                                {{-- <a href="{{ route('admin.users.index') }}" class="small-box-footer">Перейти к пользователям<i
+                                        class="fas fa-arrow-circle-right" style="margin-left: 5px"></i></a> --}}
                             </div>
-                            <a href="{{ route('admin.posts.index') }}" class="small-box-footer">Перейти к статьям<i
-                                    class="fas fa-arrow-circle-right" style="margin-left: 5px"></i></a>
+                        </div>
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-info">
+                                <div class="inner" style="text-align: center">
+                                    <p>Пользователи</p>
+                                    {{-- <h3>{{ $users->count() }}</h3> --}}
+                                </div>
+                                {{-- <a href="{{ route('admin.users.index') }}" class="small-box-footer">Перейти к пользователям<i
+                                        class="fas fa-arrow-circle-right" style="margin-left: 5px"></i></a> --}}
+                            </div>
                         </div>
                     </div>
-                    {{-- Карточка категорий --}}
-                    {{-- <div class="col-lg-3 col-6">
-                        <div class="small-box bg-info">
-                            <div class="inner" style="text-align: center">
-                                <p>Категории</p>
-                                <h3>{{ $category->count() }}</h3>
-                            </div>
-                            <div class="icon">
-                                <i class="fa-thin fa-list"></i>
-                            </div>
-                            <a href="{{ route('admin.category.index') }}" class="small-box-footer">Перейти к категориям<i
-                                    class="fas fa-arrow-circle-right" style="margin-left: 5px"></i></a>
-                        </div>
-                    </div>  --}}
                 </div>
             </div>
         </section>
