923999d3d923ced32d80194e08b2e4bcc1b8dc5e
Alex Gilev <alex.gilev.job@gmail.com>
2024-06-01 13:15:04 +0300

    pre_build

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,40 +18,30 @@ mix.js("react/src/index.js", "public/js")
         [
             "react/src/styles/reset.css",
             "react/src/styles/common.css",
-            "react/src/styles/button.css",
-            "react/src/styles/categories.css",
-            "react/src/styles/flipper.css",
             "react/src/styles/fonts.css",
-            "react/src/styles/footer.css",
-            "react/src/styles/header.css",
-            "react/src/styles/itemPage.css",
-            "react/src/styles/itemQuickBuy.css",
-            "react/src/styles/items.css",
-            "react/src/styles/modal.css",
-            "react/src/styles/offers.css",
         ],
         "public/css/app.css"
     )
     .sourceMaps();
 
-mix.webpackConfig({
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "babel-loader",
-                exclude: /node_modules/,
-                options: {
-                    presets: [
-                        "@babel/preset-env",
-                        "@babel/preset-react",
-                        "@babel/preset-typescript",
-                    ],
+    mix.webpackConfig({
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/,
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                            '@babel/preset-typescript',
+                        ],
+                    },
                 },
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-    },
-});
+            ],
+        },
+        resolve: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+    });
