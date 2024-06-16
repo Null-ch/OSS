5a98716196a2fc3dca2542d01526d029ae8d4ab6
Null-ch <zuxobot@yandex.ru>
2024-06-16 12:39:47 +0300

    upd

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/index', function () {
-    return view('public.index'); // Возвращает содержимое Blade шаблона
+    return view('public.index');
 });
 
 Route::middleware('auth')->group(function () {
