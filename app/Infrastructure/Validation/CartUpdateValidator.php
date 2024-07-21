b0fa193f4c5a3c1efc7c288173750e3a845f8029
Null-ch <zuxobot@yandex.ru>
2024-07-21 14:46:24 +0300

    validators fix

--- app/Infrastructure/Validation/CartUpdateValidator.php
+++ app/Infrastructure/Validation/CartUpdateValidator.php
@@ -6,8 +6,16 @@ use App\Infrastructure\Validation\BaseValidator;
 
 class CartUpdateValidator extends BaseValidator
 {
-    protected $rules = [
-        'cart_id' => 'required',
-        'cart' => 'required',
-    ];
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'cart_id' => 'required',
+            'cart' => 'required',
+        ];
+    }
 }
