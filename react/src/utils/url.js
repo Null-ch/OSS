13c4364b4bef0ad2460977201462c6099b20ecf5
Null-ch <zuxobot@yandex.ru>
2024-08-16 23:39:49 +0300

    Merge

--- react/src/utils/url.js
+++ react/src/utils/url.js
@@ -1,3 +1,3 @@
-export const DOMAIN = 'https://natural-soap.site/';
-// export const DOMAIN = 'http://127.0.0.1:8000/';
+// export const DOMAIN = 'https://natural-soap.site/';
+export const DOMAIN = 'http://127.0.0.1:8000/';
 //'http://127.0.0.1:8000/';
